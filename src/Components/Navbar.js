1e92b90792be2103894f0f774a1acb21ee47d4b0
themrgndz <emrgndz_pc@outlook.com>
2024-09-27 18:22:15 +0300

    FirstReactProject
    FirstReactProject

---
+++ src/Components/Navbar.js
@@ -0,0 +1,34 @@
+// src/Navbar.js
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; 
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <div className="navbar-brand">
+                <Link to="/">Uzmar Library</Link>
+            </div>
+            <ul className="navbar-links">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/books">Books</Link>
+                </li>
+                <li>
+                    <Link to="/authors">Authors</Link>
+                </li>
+                <li>
+                    <Link to="/profile">Profile</Link>
+                </li>
+                <li>
+                    <Link to="/logout">Logout</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
