fc61410612dea4e13f3a3b16e3a9f6c00a45e7b7
themrgndz <emrgndz_pc@outlook.com>
2024-10-03 17:32:00 +0300

    03.10.2024 17.31

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/Homepage";
+import Homepage from "./pages/Homepage";
 import Detail from "./pages/Detail";
 import Profile from "./pages/Profile";
+import Login from "./pages/Login"
 
 function App() {
   return (
     <div className="App">
     <Router>
       <Routes>
-        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Homepage/>}/>
         <Route path="/detail/:id" element={<Detail/>} />
         <Route path="/profile" element={<Profile/>} />
       </Routes>
